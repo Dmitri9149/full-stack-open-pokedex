b50c4ff586f9e2bbf0746c5accb667d80bf403cf
Dmitri9149 <dmitri.as.name@gmail.com>
2024-07-06 13:20:13 +0300

    add dummy endpoint for checking deployment

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
+
+app.get('/version', (req, res) => { 
+  res.send('1') // change this string to ensure a new version deployed
+})
 })
